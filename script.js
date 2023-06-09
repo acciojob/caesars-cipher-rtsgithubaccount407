7f0fc52910d0182f370c15f5e7b62cb1a477fc87
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-06-09 15:56:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,13 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
+	for(let i=0; i<encodedStr.length; i++){
+		if(lookup[encodedStr[i]] == undefined){
+			decodedArr += encodedStr[i];
+		}
+		else decodedArr += lookup[encodedStr[i]]
+	}
+return decodedArr;
   return ;//return decodedArr
 }
 
